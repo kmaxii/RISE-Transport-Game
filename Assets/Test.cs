d4be78c44e98a72d08d08c291aea286625e6f26a
kmaxi <esst936@gmail.com>
2023-11-24 15:30:48 +0100

    Revert "Added vastrafik auth as unity object"
    This reverts commit eef95225c00b196274ae514e0e9d67697c6a3714.

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -1,15 +1,21 @@
 ﻿using System.Collections.Generic;
+using System.Diagnostics;
+using Org.OpenAPITools.Api;
 using Org.OpenAPITools.Client;
 using Org.OpenAPITools.Model;
-using UnityEngine;
 
-public class VasttrafikTest : MonoBehaviour
+public class Test
 {
-    private void Start()
+    public static void Main()
     {
 
-        var detailsReference =
-            "eyJUIjpbeyJSIjoiMXwyMDExMHw0MHw4MHwyMzExMjAyMyIsIk8iOjEzLCJEIjoxNSwiSSI6MH0seyJSIjoiMXw4OTAzfDN8ODB8MjMxMTIwMjMiLCJPIjoxNSwiRCI6MTcsIkkiOjJ9XSwiQyI6W3siUiI6Ikh8MXxXJEE9MUBPPUhqYWxtYXIgQnJhbnRpbmdzcGxhdHNlbiwgR8O2dGVib3JnQEw9MzE4MDAwOEBhPTEyOEAkQT0xQE89SGphbG1hciBCcmFudGluZ3NwbGF0c2VuLCBHw7Z0ZWJvcmdATD0zMTgwMDA1QGE9MTI4QCQyMDIzMTEyMzE0NDUkMjAyMzExMjMxNDQ4JCQkMSQkJCQkJHwjVkUjMiNDRiMxMDAjQ0EjMCNDTSMwI1NJQ1QjMCNBTSM5NyNBTTIjMCNSVCMxNSMiLCJJIjoxfV19";
+        Configuration config = new Configuration();
+        config.BasePath = "https://ext-api.vasttrafik.se/pr/v4";
+        // Configure OAuth2 access token for authorization: auth
+        config.AccessToken = "YOUR_ACCESS_TOKEN";
+
+        var apiInstance = new JourneysApi(config);
+        var detailsReference = "detailsReference_example";  // string | The reference to the journey, received from the search journeys query. A detailsReference is only valid during the same day as it was generated.
         var includes = new List<VTApiPlaneraResaWebV4ModelsJourneyDetailsIncludeType>(); // List<VTApiPlaneraResaWebV4ModelsJourneyDetailsIncludeType>? | The additional information to include in the response. (optional) 
         var channelIds = new List<int>(); // List<int>? | List of channel ids to include if 'ticketsuggestions' is set in the 'includes' parameter. Optional parameter. (optional) 
         var productTypes = new List<int>(); // List<int>? | List of product type ids to include if 'ticketsuggestions' is set in the 'includes' parameter. Optional parameter. (optional) 
@@ -19,17 +25,15 @@ public class VasttrafikTest : MonoBehaviour
         try
         {
             // Returns details about a journey.
-            VTApiPlaneraResaWebV4ModelsJourneyDetailsJourneyDetailsApiModel result = VasttrafikAuth.APIInstance.JourneysDetailsReferenceDetailsGet(detailsReference, includes, channelIds, productTypes, travellerCategories);
-            Debug.Log(result);
-           // var res = VasttrafikAuth.APIInstance.JourneysGet()
+            VTApiPlaneraResaWebV4ModelsJourneyDetailsJourneyDetailsApiModel result = apiInstance.JourneysDetailsReferenceDetailsGet(detailsReference, includes, channelIds, productTypes, travellerCategories);
+            Debug.WriteLine(result);
         }
         catch (ApiException e)
         {
-            Debug.Log("Exception when calling JourneysApi.JourneysDetailsReferenceDetailsGet: " + e.Message );
-            Debug.Log("Status Code: "+ e.ErrorCode);
-            Debug.Log(e.StackTrace);
+            Debug.Print("Exception when calling JourneysApi.JourneysDetailsReferenceDetailsGet: " + e.Message );
+            Debug.Print("Status Code: "+ e.ErrorCode);
+            Debug.Print(e.StackTrace);
         }
 
     }
-    
 }
