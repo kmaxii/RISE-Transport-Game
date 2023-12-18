88ba811f87560236449e76d92a66945973304c35
kmaxi <esst936@gmail.com>
2023-12-18 10:35:57 +0100

    Added scriptable object for persona

---
+++ Assets/Scripts/Persona.cs
@@ -0,0 +1,17 @@
+using MaxisGeneralPurpose.Event;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New-Persona", menuName = "Rise/Persona")]
+public class Persona : ScriptableObject
+{
+    [Range(0, 100)] [SerializeField] private int sweatThreshold = 50;
+    [Range(0, 100)] [SerializeField] private int wetnessThreshold = 50;
+    [Range(0, 100)] [SerializeField] private int tirednessThreshold = 50;
+    [Range(0, 100)] [SerializeField] private int timeImportance = 50;
+    [Range(0, 100)] [SerializeField] private int stressImportance = 50;
+    [Range(0, 100)] [SerializeField] private int comfortImportance = 50;
+    [Range(0, 100)] [SerializeField] private int busComfortability = 50;
+    [Range(0, 100)] [SerializeField] private int tramComfortability = 50;
+    [Range(0, 100)] [SerializeField] private int eScooterComfortability = 50;
+    [Range(0, 100)] [SerializeField] private int fitness = 50;
+}
