0fd1aabe15e761fa4d616feab5b90c6e6cdb469f
Demian Skoglöv <demian.skoglow@gmail.com>
2024-01-03 10:51:00 +0100

    Changes to POI prefab so that they are no longer offset to the side

--- Assets/Mapbox/Examples/Scripts/PoiLabelTextSetter.cs
+++ Assets/Mapbox/Examples/Scripts/PoiLabelTextSetter.cs
@@ -16,7 +16,7 @@
 
 
 		[SerializeField] private Renderer inWorldImage;
-		[SerializeField] private Renderer onTopImage;
+		//[SerializeField] private Renderer onTopImage;
 		private static readonly int MainTex = Shader.PropertyToID("_MainTex");
 
 		public void Set(String text)
@@ -28,7 +28,7 @@
 		public void SetImage(Sprite sprite)
 		{
 			inWorldImage.GetComponent<SpriteRenderer>().sprite = sprite;
-			onTopImage.material.SetTexture(MainTex, sprite.texture);
+			//onTopImage.material.SetTexture(MainTex, sprite.texture);
 		}
 
 		public void SetBackgroundColor(Color color)
