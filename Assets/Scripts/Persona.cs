bdf5fbee62766e576317920b6446cdbdc6600379
a19oskbl <a19oskbl@student.his.se>
2024-01-18 12:46:43 +0100

    Update Persona.cs

--- Assets/Scripts/Persona.cs
+++ Assets/Scripts/Persona.cs
@@ -6,12 +6,12 @@ public class Persona : ScriptableObject
     [Range(0, 100)] [SerializeField] private int sweatThreshold = 50;
     [Range(0, 100)] [SerializeField] private int wetnessThreshold = 50;
     [Range(0, 100)] [SerializeField] private int tirednessThreshold = 50;
-    [Range(0, 100)] [SerializeField] private int timeImportance = 50;
-    [Range(0, 100)] [SerializeField] private int stressImportance = 50;
-    [Range(0, 100)] [SerializeField] private int comfortImportance = 50;
-    [Range(0, 100)] [SerializeField] private int busComfortability = 50;
-    [Range(0, 100)] [SerializeField] private int tramComfortability = 50;
-    [Range(0, 100)] [SerializeField] private int eScooterComfortability = 50;
+    [Range(1, 5)] [SerializeField] private int timeImportance = 0;
+    [Range(1, 5)] [SerializeField] private int stressImportance = 0;
+    [Range(1, 5)] [SerializeField] private int comfortImportance = 0;
+    [Range(-1, 5)] [SerializeField] private int busComfortability = 0;
+    [Range(-1, 5)] [SerializeField] private int tramComfortability = 0;
+    [Range(-1, 5)] [SerializeField] private int eScooterComfortability = 0;
     [Range(0, 100)] [SerializeField] private int fitness = 50;
     
 }
