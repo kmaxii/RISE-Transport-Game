cd257ae565685760dda30e63090ec04bea2e9da6
kmaxii <esst936@gmail.com>
2024-01-19 16:30:25 +0100

    Added a price show Ui for scooters (doesn't show yet)

---
+++ Assets/ShowEscooterPriceUi.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Globalization;
+using Interfaces;
+using MaxisGeneralPurpose.Scriptable_objects;
+using Scriptable_objects;
+using TMPro;
+using UnityEngine;
+
+public class ShowEscooterPriceUi : MonoBehaviour, IEventListenerInterface
+{
+    [SerializeField] private FloatVariable initScooterPrice;
+    [SerializeField] private FloatVariable pricePerMin;
+    [SerializeField] private GameEvent showScooterUiEvent;
+    [SerializeField] private GameEvent mountScooterEvent;
+
+    [SerializeField] private TMP_Text text;
+    private String _initialText;
+
+    private void Awake()
+    {
+        _initialText = text.text;
+    }
+
+    private void OnEnable()
+    {
+        showScooterUiEvent.RegisterListener(this);
+    }
+
+    private void OnDisable()
+    {
+        showScooterUiEvent.UnregisterListener(this);
+    }
+
+    private void ShowChildren(bool value)
+    {
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            transform.GetChild(i).gameObject.SetActive(value);
+        }
+    }
+    
+    public void OnEventRaised()
+    {
+        text.text = _initialText.Replace("<InitPrice>", initScooterPrice.Value + "")
+            .Replace("<perMinPrice>", pricePerMin.Value + "");
+        ShowChildren(true);
+    }
+}
