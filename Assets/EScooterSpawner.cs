32a842d4359a8402ef74c85d76cb1103c79fbc9a
kmaxii <esst936@gmail.com>
2024-01-30 11:22:35 +0100

    Made scooters spawn at more random position and added a variable to change spawn Chance

--- Assets/EScooterSpawner.cs
+++ Assets/EScooterSpawner.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using MaxisGeneralPurpose.Scriptable_objects;
 using UnityEngine;
+using UnityEngine.Pool;
 
 public class EScooterSpawner : MonoBehaviour
 {
@@ -12,6 +13,9 @@ public class EScooterSpawner : MonoBehaviour
 
     private Dictionary<Vector2Int, List<GameObject>> _spawnedEScootersInGrids;
 
+    private ObjectPool<GameObject> _eScooterPool;
+
+
     [Header("Spawn Settings")]
     [Tooltip("The amount of 32x32 grids to spawn eScooters in. 0 = only spawn in the current grid.")]
     [SerializeField]
@@ -19,6 +23,10 @@ public class EScooterSpawner : MonoBehaviour
 
     [Tooltip("The maximum amount of eScooters to spawn in a 32x32 grid.")] [SerializeField]
     private int maxSpawnPerGrid = 30;
+
+    //SpawnChance
+    [Tooltip("The chance for each scooter that it wants to spawn to actually spawn.")] [SerializeField] [Range(0, 1)]
+    private float spawnChance = 1f;
 
     private void OnEnable()
     {
@@ -33,7 +41,35 @@ public class EScooterSpawner : MonoBehaviour
     private void Awake()
     {
         _spawnedEScootersInGrids = new Dictionary<Vector2Int, List<GameObject>>();
+
+        // Initialize the object pool
+        _eScooterPool = new ObjectPool<GameObject>(
+            createFunc: () =>
+            {
+                return Instantiate(eScooterPrefab); // Create new eScooter
+            },
+            actionOnGet: (eScooter) =>
+            {
+                eScooter.SetActive(true); // Activate eScooter when taken from the pool
+            },
+            actionOnRelease: (eScooter) =>
+            {
+                eScooter.SetActive(false); // Deactivate eScooter when returned to the pool
+            },
+            actionOnDestroy: (eScooter) =>
+            {
+                Destroy(eScooter); // Destroy eScooter when the pool is cleared
+            },
+            collectionCheck: false, // Set this to true only if you need to check for double-release etc.
+            defaultCapacity: 20, // Initial pool size
+            maxSize: 50 // Maximum number of objects that can be in the pool
+        );
     }
+
+    /**
+     * Cached list so that not a new one needs to be created every time
+     */
+    private readonly List<Vector2Int> _gridsToRemove = new List<Vector2Int>();
 
     private void SpawnEScooters()
     {
@@ -41,23 +77,22 @@ public class EScooterSpawner : MonoBehaviour
         int playerXGrid = (int) Math.Floor(playerPos.x / 32);
         int playerYGrid = (int) Math.Floor(playerPos.z / 32);
 
-        // Track grids to remove
-        List<Vector2Int> gridsToRemove = new List<Vector2Int>();
+        _gridsToRemove.Clear();
 
         foreach (var grid in _spawnedEScootersInGrids.Keys)
         {
             if (Mathf.Abs(grid.x - playerXGrid) > spawnDistance || Mathf.Abs(grid.y - playerYGrid) > spawnDistance)
             {
-                gridsToRemove.Add(grid);
+                _gridsToRemove.Add(grid);
             }
         }
 
         // Remove scooters in grids outside of spawn distance
-        foreach (var grid in gridsToRemove)
+        foreach (var grid in _gridsToRemove)
         {
             foreach (var scooter in _spawnedEScootersInGrids[grid])
             {
-                Destroy(scooter);
+                _eScooterPool.Release(scooter);
                 Debug.Log($"Destroyed eScooter in grid {grid.x}, {grid.y}");
             }
 
@@ -76,28 +111,27 @@ public class EScooterSpawner : MonoBehaviour
                 if (!_spawnedEScootersInGrids.ContainsKey(gridKey))
                 {
                     _spawnedEScootersInGrids[gridKey] = new List<GameObject>();
-
-                    Debug.Log($"Spawning eScooters in grid {xGrid}, {yGrid}");
-
                     SpawnEScootersInGrid(xGrid, yGrid, _spawnedEScootersInGrids[gridKey]);
                 }
             }
         }
     }
 
+
     private void SpawnEScootersInGrid(int xGrid, int yGrid, List<GameObject> scootersInGrid)
     {
         List<EScooterData.GridInfo> gridData = eScooterData.GetData(xGrid, yGrid);
 
+        scootersInGrid.Shuffle();
+
         for (var i = 0; i < Math.Min(gridData.Count, maxSpawnPerGrid); i++)
         {
+            if (UnityEngine.Random.value > spawnChance) continue;
             var gridInfo = gridData[i];
-            if (scootersInGrid.Find(scooter => scooter.GetInstanceID() == gridInfo.entryId) == null)
-            {
-                GameObject eScooter = Instantiate(eScooterPrefab);
-                eScooter.transform.position = new Vector3((float) gridInfo.xPos, 0, (float) gridInfo.yPos);
-                scootersInGrid.Add(eScooter);
-            }
+
+            GameObject eScooter = _eScooterPool.Get();
+            eScooter.transform.position = new Vector3((float) gridInfo.xPos, 0, (float) gridInfo.yPos);
+            scootersInGrid.Add(eScooter);
         }
     }
 }
