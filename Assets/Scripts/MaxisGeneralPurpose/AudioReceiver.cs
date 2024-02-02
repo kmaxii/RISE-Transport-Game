5d06b29aa00b9ebfeeee9cec92d0ad2a364cb78f
kmaxii <esst936@gmail.com>
2024-02-02 16:50:08 +0100

    Added a StopAudio Function

--- Assets/Scripts/MaxisGeneralPurpose/AudioReceiver.cs
+++ Assets/Scripts/MaxisGeneralPurpose/AudioReceiver.cs
@@ -3,42 +3,61 @@ using MaxisGeneralPurpose.Scriptable_objects;
 using Scriptable_objects;
 using UnityEngine;
 
-public class AudioReceiver : MonoBehaviour
+namespace MaxisGeneralPurpose
 {
-
-    private readonly List<AudioSource> _audioSources = new List<AudioSource>();
+    public class AudioReceiver : MonoBehaviour
+    {
+        private readonly List<AudioSource> _audioSources = new();
+    
     
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (_audioSources.Count > 10)
+
+        // Update is called once per frame
+        void Update()
         {
-            AudioSource audioSource = _audioSources[10];
-            if (!audioSource.isPlaying)
+            if (_audioSources.Count > 10)
             {
-                Destroy(audioSource);
-                _audioSources.RemoveAt(10);
+                AudioSource audioSource = _audioSources[10];
+                if (!audioSource.isPlaying)
+                {
+                    Destroy(audioSource);
+                    _audioSources.RemoveAt(10);
+                }
             }
         }
-    }
 
 
-    public void OnReceiveAudio(AudioEvent audioEvent)
-    {
-        //Searches for a not playing audio source
-        for (int i = 0; i < _audioSources.Count; i++)
+        public void OnReceiveAudio(AudioEvent audioEvent)
         {
-            AudioSource audioSource = _audioSources[i];
-            if (audioSource.isPlaying)
-                continue;
-            //Found unused audio source. Playing using that
-            audioEvent.Play(audioSource);
-            return;
+            //Searches for a not playing audio source
+            for (int i = 0; i < _audioSources.Count; i++)
+            {
+                AudioSource audioSource = _audioSources[i];
+                if (audioSource.isPlaying)
+                    continue;
+                //Found unused audio source. Playing using that
+                audioEvent.Play(audioSource);
+                return;
+            }
+        
+            //No unused audio source was found. So adding a new one.
+            _audioSources.Add(gameObject.AddComponent<AudioSource>());
+            audioEvent.Play(_audioSources[^1]);
         }
         
-        //No unused audio source was found. So adding a new one.
-        _audioSources.Add(gameObject.AddComponent<AudioSource>());
-        audioEvent.Play(_audioSources[^1]);
+        public void StopAudio(SimpleAudioPlayer audioEvent)
+        {
+            foreach (var audioSource in _audioSources)
+            {
+                foreach (var audioEventAudioClip in audioEvent.audioClips)
+                {
+                    if (audioSource.clip == audioEventAudioClip)
+                    {
+                        audioSource.Stop();
+                        return;
+                    }
+                }
+            }
+        }
     }
 }
